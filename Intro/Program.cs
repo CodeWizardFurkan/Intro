5495a43d3c27f3d6436c04b19b7dcd696ab8bc90
Furkan <furkandemir.official0@gmail.com>
2024-02-05 10:49:29 +0300

    Add project files.

---
+++ Intro/Program.cs
@@ -0,0 +1,64 @@
+﻿using Intro.Business;
+using Intro.Entities;
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+bool isAsuthenticated = false;
+Console.WriteLine(message1);
+
+if (isAsuthenticated)
+{
+    Console.WriteLine("Buton-->Hoşgeldin Furkan");
+}
+else
+{
+    Console.WriteLine("Buton-->Sisteme giriş yap");
+}
+
+string[] loans = {"Kredi 1", "Kredi 2", "Kredi 3", "Kredi 4", "Kredi 5", "Kredi 6" };
+//string[] loans2 = new string[6];
+//loans2[0] = "Kredi 1";
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C#";
+course1.Description = ".Net 8 vs...";
+course1.Price = 0;
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "Java";
+course2.Description = "Java 17 vs...";
+course2.Price = 10;
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "Python";
+course3.Description = "Python 1.12 vs...";
+course3.Price = 20;
+
+Course[] courses = { course1, course2, course3 };
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " / " + courses[i].Price);
+}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " / " + courses2[i].Price);
+}
+
+Console.WriteLine("Kod bitti");
+
+
